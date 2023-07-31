53251ad560a4201e6ae68f287b0a598128f0d368
Artyom <@artemkim953@gmail.com>
2023-07-31 16:56:04 +0600

    final2

--- pages/lending/slider/slider.js
+++ pages/lending/slider/slider.js
@@ -1,44 +1,18 @@
-import Image from 'next/image';
-import React, { useState, useEffect } from 'react';
-import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
-import { RxDotFilled } from 'react-icons/rx';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation'
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
-import FirstImage from '../../../public/патруль.png'
-import SecondImage from '../../../public/патруль_1.png'
+// import Image from 'next/image';
+// import React, { useState, useEffect } from 'react';
+// import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+// import { RxDotFilled } from 'react-icons/rx';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/css';
+// import 'swiper/css/pagination';
+// import 'swiper/css/navigation'
+// import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+// import FirstImage from '../../../public/патруль.png'
+// import SecondImage from '../../../public/патруль_1.png'
 const ImageSlider = () => {
   return (
     <>
-      <Swiper
-
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 25000000,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="h-2/3"
-      >
-        <SwiperSlide>
-          <Image src={FirstImage}  alt="as"/>
-        </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
+      <div>Slider</div>
     </>
   );
 }
