5a26cfb083d2ff82f1d0305a8065d7bd1409c5fe
Artyom <@artemkim953@gmail.com>
2023-08-01 08:34:37 +0600

    final7

--- config/api.config.ts
+++ config/api.config.ts
@@ -1,2 +1,2 @@
 export const API_URL = `https://patrol-backend-service.onrender.com/server`
-export const API_SERVER_URL = `https://patrol-backend-service.onrender.com/server`
+export const API_SERVER_URL = `https://patrol-backend-service.onrender.com/server/`
