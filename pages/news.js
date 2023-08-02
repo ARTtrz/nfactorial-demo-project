6d095cfa71638b1ab85dd0b2325735d44fe9bab2
Artyom <@artemkim953@gmail.com>
2023-08-02 20:49:40 +0600

    finish17

--- pages/news.js
+++ pages/news.js
@@ -31,7 +31,7 @@ export default function NewsPage() {
                 <div className="max-w-[1550px] m-auto">
                     <Header/>
                     {/* <h1 className="text-4xl px-4 pt-4 font-medium">Новости</h1> */}
-                    <div className="container mx-auto px-4 py-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+                    <div className="w-auto mx-auto px-4 py-8 grid grid-cols-4 sm:grid-cols-2 md:grid-cols-4 gap-4">
                         {
                             data.map((curElem) => {
                                 return <NewsCard key={curElem.publish_date} link={curElem.link} title={curElem.title} imageUrl={curElem.image_url} date={curElem.publish_date}/>
