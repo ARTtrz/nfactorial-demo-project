4534ee19d27801d498887fbacda63375af9b6e77
Artyom <@artemkim953@gmail.com>
2023-08-03 10:26:43 +0600

    finish18

--- pages/lending/main.js
+++ pages/lending/main.js
@@ -10,20 +10,20 @@ export default function Main(){
     const router = useRouter()
     return(
         <main className="mt-6" >
-            <div className=" px-10 pt-16  pb-3 max-w-[1600px] m-auto bg-white rounded-lg">
+            <div className=" px-10 pt-16  pb-3 max-w-[1550px] m-auto bg-white rounded-lg">
                 <div className="flex items-center justify-between">
                     <div className="flex flex-col items-start justify-between gap-5" >
                         <h1 className="uppercase text-3xl font-medium ">
                             Устал ездить без прав?
                         </h1>
 
-                        <span className="text-xl max-w-[100%] ">
-                        Добро пожаловать в будущее ГИБДД! Наш чат-бот на искусственном интеллекте упростит взаимодействие с правоохранительными органами. Получайте быстрые ответы о правилах, штрафах и регистрации. Удобство и эффективность - в одном приложении!
+                        <span className="text-xl max-w-[600px] ">
+                            Добро пожаловать в будущее ГИБДД! Наш чат-бот на искусственном интеллекте упростит взаимодействие с правоохранительными органами. Получайте быстрые ответы о правилах, штрафах и регистрации. Удобство и эффективность - в одном приложении!
                         </span>
                         <button type="button" onClick={() => router.push('/chatbot')} className="text-white   bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-lg  px-10 py-4  text-center mr-2 mb-2">Перейти</button>
 
                     </div>
-                    <Image src={MainImage} alt="image" width={950} />
+                    <Image src={MainImage} alt="image" width={826} />
                 </div>
             </div>
             <div className=" px-10 mt-10 py-4 max-w-[1600px] justify-between m-auto gap-5 bg-white flex items-center">
